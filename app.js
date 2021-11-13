e05621d903f1eccd82bac02c38bc5922831df3a7
Earthling <dmitshash@yandex.ru>
2021-11-13 21:38:51 +0200

    feature: добавить логгер

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const rootRouter = require('./routes/index');
 const errorHandler = require('./middlewares/errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -20,7 +21,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 // app.use(express.static(path.resolve(__dirname, './public')));
 app.use(express.json());
 app.use(cookieParser());
+app.use(requestLogger);
 app.use('/', rootRouter);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
